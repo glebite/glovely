64df2b2a1c2eb4974c6c08deb1ff51417134a0be
Lazlo <glebite@gmail.com>
2019-10-04 21:13:27 -0400

    Cleaned out some unused code, added comments

--- src/glovely.ino
+++ src/glovely.ino
@@ -1,44 +1,44 @@
-// What pins to connect the sensors to
-// originally "Flex_Sensor_Glove.ino" from
-// https://www.instructables.com/id/Arduino-Flex-Sensor-Glove/
+/*
+	glovely.ino
 
-#define FLEXSENSORPIN0 A0 
-#define FLEXSENSORPIN1 A1 
+	A flex-sensor driven glove to allow for bluetooth controls over a
+	presentation system and built-in laser pointer and extra fun!
+
+	The circuit:
+	-
+	-
+	-
+
+	Copied 2019-10-04
+	https://www.instructables.com/id/Arduino-Flex-Sensor-Glove/
+	Modified 2019-10-04
+	Me.
+
+	https://github.com/glebite/glovely
+*/
+
+
 #define FLEXSENSORPIN2 A2 
 #define FLEXSENSORPIN3 A3 
 #define FLEXSENSORPIN4 A4 
 
-//WILL HAVE TO CHANGE ONCE ON GLOVE AS PINS WILL LIKELY CHANGE
-int ledAnalogOne[] = {9, 3, 6}; //the three pins of the first analog LED in the order of red, green, blue
-                                //These pins must be PWM
-
 //defined variables for each sensor according to finger
-float pinkyReading;
-float ringReading;
 float middleReading;
 float indexReading;
 float thumbReading;
  
 void setup(void) {
   Serial.begin(9600);
-  for(int i = 0; i < 3; i++){
-   pinMode(ledAnalogOne[i], OUTPUT);   //Set the three LED pins as outputs
+  for(int thisPin = 0; thisPin < 3; thisPin++){
+   pinMode(ledAnalogOne[thisPin], OUTPUT);   //Set the three LED pins as outputs
   }
 }
  
 void loop(void) {
-  pinkyReading = singleReading(FLEXSENSORPIN1);
-  ringReading = singleReading(FLEXSENSORPIN0);
   middleReading = singleReading(FLEXSENSORPIN2);
   indexReading = singleReading(FLEXSENSORPIN3);
   thumbReading = singleReading(FLEXSENSORPIN4);
   
-  Serial.print("Single reading Pinky "); 
-  Serial.println(pinkyReading);
-
-  Serial.print("Single reading Ring "); 
-  Serial.println(ringReading);
-
   Serial.print("Single reading Middle "); 
   Serial.println(middleReading);
 
@@ -47,15 +47,6 @@ void loop(void) {
 
   Serial.print("Single reading Thumb "); 
   Serial.println(thumbReading);
-
-  /*map 3 readings to be RBG values. The bottom and top values were taken by analyzing multiple readings to determine the best values to use.
-  These values will likely be different for everyone so be sure to change them as they suit your project
-  can use average() to determine the values as well*/
-  ringReading = mapReading(ringReading,200,580);
-  pinkyReading = mapReading(pinkyReading,205,430);
-  indexReading = mapReading(indexReading,280,560);
-  
-  setLED(pinkyReading, ringReading, indexReading);
 
   delay(1000);
 }
@@ -97,27 +88,6 @@ float mapReading(float analogReading, float bottom, float top)
     calibratedValue = map(analogReading, bottom,top,0,254); 
   }
   return calibratedValue;
-}
-
-/**
- * sets the values for the RBG LED
- * @param red the value for the red light 0-255
- * @param green the value for the green light 0-255
- * @param blue the value for the blue light 0-255
- */
-void setLED(float red, float green, float blue)
-{
-   Serial.println("");
-   Serial.print("Red: ");
-   Serial.println(red);
-   Serial.print("Green: ");
-   Serial.println(green);
-   Serial.print("Blue: ");
-   Serial.println(blue);
-   Serial.println("");
-   analogWrite(ledAnalogOne[0], red);
-   analogWrite(ledAnalogOne[1], green);
-   analogWrite(ledAnalogOne[2], blue);
 }
 
 /**
